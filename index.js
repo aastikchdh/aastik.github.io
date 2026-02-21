0057e25a38f3b20fa08bd2131b0ef47a3f441e01
aastikchdh <aastik.choudhary@gmail.com>
2026-02-22 00:39:10 +0530

    Added some basic animations using JS

---
+++ index.js
@@ -0,0 +1,17 @@
+const heading1 = document.getElementById("main-heading1");
+const heading2 = document.getElementById("main-heading2");
+function fade(element){
+    let opacity = 0;
+    let interval = setInterval(() => {
+        opacity += 0.05;
+        element.style.opacity = opacity;
+        if(opacity >= 1)
+        {
+            clearInterval(interval);
+        }
+    }, 100);
+}
+fade(heading1);
+setInterval(()=>{
+    fade(heading2);
+},100)
